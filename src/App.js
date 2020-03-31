dd0b6fac9639c5a5eb7f9f36ffe6ab8547be8564
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-03-31 16:06:33 +0300

    send data from search component to app and save it in its state to use it later in News Cpmponent #7 #3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import News from "./Components/News";
 import Search from "./Components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <Search />
-      <News />
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { articles: [] };
+  }
 
+  displayArticles = (articles) => {
+    this.setState({
+      articles
+    });
+    console.log(this.state.articles);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Search displayArticles={this.displayArticles} />
+        <News />
+      </div>
+    );
+  }
+}
 export default App;
