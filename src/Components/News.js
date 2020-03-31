67887d5741891e9df1a88a419bffd7e0abb5d89a
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-03-31 15:24:52 +0300

    add form to search component, fix the key warning in news component #3 #4

---
+++ src/Components/News.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lists: []
+    };
+  }
+
+  componentDidMount() {
+    const newsAPI =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=626633f093cd40b7bda4ca1a94cc2b89";
+    fetch(newsAPI)
+      .then((res) => res.json())
+
+      .then((res) => {
+        this.setState({ lists: res.articles });
+      })
+
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.lists.map((item, i) => {
+          return (
+            <ul>
+              <li key={i}>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <button>translate</button>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default News;
